0799e9083107fd9d03dba57737a24c89a0383b33
codercaizh <545347837@qq.com>
2025-09-11 01:16:57 +0800

    新增xfrpc

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -42,3 +42,6 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-lucky lucky"
 # 集客AC
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-gecoosac gecoosac"
+
+
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-xfrpc-zh-cn"
